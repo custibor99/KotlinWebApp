d9c88829a0b1f5ad2295939532a459d7bb6c37f7
= <=>
2022-10-02 16:28:06 +0200

    Adding ping class and Get endpoint.

--- src/main/kotlin/com/example/demo/controler/UtilityController.kt
+++ src/main/kotlin/com/example/demo/controler/UtilityController.kt
@@ -1,4 +1,26 @@
 package com.example.demo.controler
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+class Ping(){
+    companion object {
+        var counter = 0
+
+        fun getPingCounter():Int {
+            counter++
+            return counter
+        }
+    }
+}
+
+@RestController
+@RequestMapping("api/v1/")
 class UtilityController {
+
+    @GetMapping("ping")
+    fun ping(): Int{
+        return Ping.getPingCounter()
+    }
 }
